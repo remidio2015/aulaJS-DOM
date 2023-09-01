0143c64ae403a72c989e46f4c120dc3ceaeb5aec
remidio2015 <remidio2030@gmail.com>
2023-09-01 20:49:24 -0300

    projeto lampada JS

---
+++ projeto-lampada/lampada.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+const turnOn = document.getElementById( 'turnOn' );
+const turnOff = document.getElementById( 'turnOff' );
+const lamp = document.getElementById( 'lamp' );
+
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function lampOn() {
+    if (!isLampBroken ()) {
+        lamp.src='./img/ligada.jpg';
+    }
+}
+
+function lampOff() {
+    if (!isLampBroken ()) {
+        lamp.src='./img/desligada.jpg';
+    }
+}
+
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
